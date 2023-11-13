8ef45bc2ecf8ddb3f26bbd62e2760589073c761d
Zolay <75842308+zolaygans@users.noreply.github.com>
2023-11-13 20:57:37 +0700

    identitas.java

--- identitas.java
+++ identitas.java
@@ -1,26 +1,23 @@
-import java.util.Random;
-
 public class PerkenalanDiri {
 
   public static void main(String[] args) {
 
-    String[] hobies = {"Berenang","Travelling"};
-
-    String nama = "Sandi Winata";
+    String nama = "Sandi Winata"; 
+    int umur = 18; 
+    float tinggiBadan = 165;
+    
     String prodi = "Ilmu Komputer";
     String nim = "I.2310247";
     String fakultas = "FILKOM";
-    
+
     System.out.println("Halo!");
-    System.out.println("Perkenalkan nama saya " + nama); 
+    System.out.println("Perkenalkan nama saya " + nama);
+
+    System.out.println("Umur saya " + umur + " tahun");
+    System.out.println("Tinggi badan saya " + tinggiBadan + " cm");
     
     System.out.println("Saya adalah mahasiswa " + prodi + " dengan NIM " + nim);
     System.out.println("di Fakultas " + fakultas);
-    
-    System.out.println("Beberapa hobi saya:");
-    for(String hobi : hobies) {
-      System.out.println("- " + hobi);
-    }
 
   }
 
