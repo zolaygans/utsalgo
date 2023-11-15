d06f8ebfb6c9922729713dc823245c3fa633b66c
Zolay <75842308+zolaygans@users.noreply.github.com>
2023-11-15 22:19:21 +0700

    identitas.java

--- identitas.java
+++ identitas.java
@@ -1,24 +1,27 @@
 public class PerkenalanDiri {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String nama = "Sandi Winata"; 
-    int umur = 18; 
-    float tinggiBadan = 165;
+        String nama = "Sandi Winata";
+        String ttl = "31 Maret 2005";
+        int umur = 18;
+        float tinggiBadan = 165;
     
     String prodi = "Ilmu Komputer";
-    String nim = "I.2310247";
-    String fakultas = "FILKOM";
+        String nim = "I.2310247";
+        String fakultas = "FILKOM";
+        String hobi = "Berenang";
 
-    System.out.println("Halo!");
-    System.out.println("Perkenalkan nama saya " + nama);
+        System.out.println("Assalamualikum Wr. Wb.");
+        System.out.println("Perkenalkan nama saya " + nama);
+        System.out.println("Tanggal lahir saya" + ttl);
 
-    System.out.println("Umur saya " + umur + " tahun");
-    System.out.println("Tinggi badan saya " + tinggiBadan + " cm");
-    
-    System.out.println("Saya adalah mahasiswa " + prodi + " dengan NIM " + nim);
-    System.out.println("di Fakultas " + fakultas);
+        System.out.println("Umur saya " + umur + " tahun");
+        System.out.println("Tinggi badan saya " + tinggiBadan + " cm");
+        System.out.println("Hobi saya " + hobi);
+        System.out.println("Saya adalah mahasiswa " + prodi + " dengan NIM " + nim);
+        System.out.println("di Fakultas " + fakultas);
 
-  }
+    }
 
 }
